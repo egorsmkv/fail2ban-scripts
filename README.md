22f24e06527d1635b84e91a601a5775c94fae396
Yehor Smoliakov <yehors+gh@ukr.net>
2021-08-30 23:05:00 +0300

    Add unban.py and changed the API of scripts

--- README.md
+++ README.md
@@ -1,16 +1,39 @@
-# Fail2ban API
+# Fail2ban Scripts
 
-## Usage
+## Usage `banned_list.py`
 
-This script tries to get the banned list of IP addresses by Fail2ban for service `freeswitch`.
+This script tries to get the banned list of IP addresses by Fail2ban for the service `freeswitch`.
+
 You can modify the service name if you want to.
 
 ```bash
-python ./banned_list.py
+python ./banned_list.py freeswitch
 ```
 
-In the response you will get JSON output with the following:
+In the response you will get JSON output with the following (data can be different, of course):
 
 ```json
 {"currently_banned": 11, "total_banned": 15, "banned_ips": ["185.108.106.105", "193.46.255.195", "193.46.255.214", "196.196.203.86", "52.232.132.24", "103.145.13.81", "23.148.145.210", "103.145.13.247", "141.98.10.188", "45.34.5.54", "40.86.206.158"]}
+```
+
+## Usage `unban.py`
+
+This script tries to unban an IP address.
+
+You can modify the IP address if you want to.
+
+```bash
+python ./unban.py 185.108.106.105
+```
+
+In the response you will get JSON output with the following (data can be different, of course):
+
+```json
+{"success": true}
+```
+
+or this (if the IP address is incorrect):
+
+```json
+{"error": true, "reason": "it seems IP does not exist"}
 ```
--- unban.py
+++ unban.py
@@ -51,46 +51,19 @@ def send_command(command):
         return error('exception occurred: ' + str(e))
 
 
-def extract_stats(data):
-    """
-    This function extracts needed data from "status" command.
-
-    :param data: Response from the server
-    :return: Needed data
-    """
-
-    n_data = {
-        'currently_banned': 0,
-        'total_banned': 0,
-        'banned_ips': [],
-    }
-
-    for k, v in data:
-        if k == 'Actions':
-            for k2, v2 in v:
-                if k2 == 'Currently banned':
-                    n_data['currently_banned'] = v2
-
-                if k2 == 'Total banned':
-                    n_data['total_banned'] = v2
-
-                if k2 == 'Banned IP list':
-                    n_data['banned_ips'] = v2
-
-    return n_data
-
-
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Incorrect number of arguments')
         exit(1)
 
-    cmd = ['status', sys.argv[1]]
+    cmd = ['unban', sys.argv[1]]
 
     response = send_command(cmd)
 
     if response['error']:
         print(json.dumps(response))
     else:
-        stats = extract_stats(response['data'])
-        print(json.dumps(stats))
+        if response['data'] == 1:
+            print(json.dumps({'success': True}))
+        else:
+            print(json.dumps(error('it seems IP does not exist')))
